157191723edc0aa106881a0f70c2e63cf3fa54d2
Tomas Alvarez <l.aalt011003@itses.edu.mx>
2023-10-05 13:39:18 -0600

    Creacion de componente menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AgregarComponent } from './agregar/agregar.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AgregarComponent
+    AgregarComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
