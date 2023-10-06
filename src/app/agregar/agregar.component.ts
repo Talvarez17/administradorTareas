3409033c1c7c5376073377f3616c4759a9b901a8
Tomas Alvarez <l.aalt011003@itses.edu.mx>
2023-10-06 13:23:04 -0600

    Diseño y funcionabilidad del menu y agregar actividad

--- src/app/agregar/agregar.component.ts
+++ src/app/agregar/agregar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agregar',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AgregarComponent {
 
+  Formulario: FormGroup = this.fb.group({
+    titulo: [, [Validators.required, Validators.maxLength(99)]],
+    descripcion: [, [Validators.required, Validators.maxLength(255)]],
+    fecha: [, Validators.required],
+    estado: [,Validators.required]   
+  });
+
+  constructor(private fb: FormBuilder, private router: Router ) { }
+
+  campoEsValido(campo: string) {
+    return this.Formulario.controls[campo].errors && this.Formulario.controls[campo].touched;
+  }
+
+  guardar() {
+    localStorage.setItem('tarea', JSON.stringify(this.Formulario.value));
+    console.log(this.Formulario.value);
+  }
+    
+
 }
+
